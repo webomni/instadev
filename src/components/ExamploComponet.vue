ae8204f435637ab9cace68880a117959ab54c47c
webomni <prpaulohcmg@hotmail.com>
2023-10-07 20:05:14 -0300

    Clear all projeto

--- src/components/ExamploComponet.vue
+++ src/components/ExamploComponet.vue
@@ -1,14 +1,6 @@
 <template>
-  <q-item
-    clickable
-    tag="a"
-    target="_blank"
-    :href="link"
-  >
-    <q-item-section
-      v-if="icon"
-      avatar
-    >
+  <q-item clickable tag="a" target="_blank" :href="link">
+    <q-item-section v-if="icon" avatar>
       <q-icon :name="icon" />
     </q-item-section>
 
@@ -23,7 +15,7 @@
 
 <script>
 export default {
-  name: 'EssentialLink',
+  name: 'ExamploComponet',
   props: {
     title: {
       type: String,
